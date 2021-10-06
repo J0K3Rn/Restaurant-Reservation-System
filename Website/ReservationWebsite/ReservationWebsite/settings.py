99ab591f0de1abb453e7e1a2e512ba321682f92e
Thien Bui <thienbui@Thiens-MacBook-Air.local>
2021-10-05 20:17:00 -0500

    add dj-reservation middleware

--- Website/ReservationWebsite/ReservationWebsite/settings.py
+++ Website/ReservationWebsite/ReservationWebsite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'djreservation.middleware.ReservationMiddleware'
 ]
 
 ROOT_URLCONF = 'ReservationWebsite.urls'
